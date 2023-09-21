3c056a9aa59ee5472359e0eb4e3fac77f3876b90
Alexander Rasoli <alexander.r2000@hotmail.com>
2023-09-21 16:23:11 +0200

    projects fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,73 +1,65 @@
 import React from "react";
 import Tilt from "react-tilt";
-import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({ index, name, description, tags, image, demo }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-7 rounded-2xl sm:w-[360px] w-full md:p-5 ">
-        <div className="relative w-full h-[230px] ">
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
-          />
-          <div className="mt-5">
-            <h3 className="text-white font-bold text-[24px] ">{name}</h3>
-            <p className="mt-2 text-secondary text-[14px] ">{description}</p>
-          </div>
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <a
-              href={demo}
-              target="_blank"
-              className="black-gradient w-20 h-10 rounded-full flex justify-center items-center cursor-pointer hover:opacity-30 transition-opacity">
-              Demo
-            </a>
-          </div>
-        </div>
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full ">
+      <div className="relative w-full h-[230px] ">
+        <img
+          src={image}
+          alt="project_image"
+          className="w-full h-full object-cover rounded-2xl"
+        />
 
-        <div className=" flex flex-wrap gap-2 md:mt-16 mt-1">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
-            </p>
-          ))}
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <a
+            href={demo}
+            target="_blank"
+            className="black-gradient w-20 h-10 rounded-full flex justify-center items-center cursor-pointer hover:opacity-30 transition-opacity">
+            Demo
+          </a>
         </div>
-      </Tilt>
-    </motion.div>
+      </div>
+      <div className="mt-3">
+        <h3 className="text-white font-bold text-[24px] ">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px] ">{description}</p>
+      </div>
+      <div className=" mt-3 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </Tilt>
   );
 };
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <div>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      </div>
 
       <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project has a link to a live demo
           in it. It reflects my ability to solve complex problems, work with
           different technologies, and manage projects effectively.
-        </motion.p>
+        </p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7 justify-center">
